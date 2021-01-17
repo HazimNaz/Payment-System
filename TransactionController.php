cc5cc3c1f3d908b930eadc2b65f156486f9bd202
HazimX <43401194+HazimX@users.noreply.github.com>
2021-01-18 03:48:58 +0800

    Update TransactionController.php

--- TransactionController.php
+++ TransactionController.php
@@ -62,9 +62,10 @@ class TransactionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($transaction_id)
     {
-        //
+        $onlinebanking = OnlineBankingDB::all();
+        return view('transaction.show',compact('onlinebanking'));
     }
 
     /**
