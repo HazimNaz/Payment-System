137099e1d1e3971d7f67a3ca01d1eb90d92aeb7e
HazimX <43401194+HazimX@users.noreply.github.com>
2021-01-18 03:46:22 +0800

    Update show.blade.php

--- payment/show.blade.php
+++ payment/show.blade.php
@@ -1,2 +1,54 @@
 <title>Payment System</title>
+@extends('nav')
+
+
+@section('content')
+
+<div class="col-sm-12">
+    <br>
+    <h3 class="display-5 text-center">Credit Card Detail</h3>    
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                 <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        <br /> 
+    @endif
+      <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Payment ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Paid Amount</th>
+                <th>Card Number</th>
+                <th>Card CVC</th>
+                <th>Card Exp Month</th>
+                <th>Card Exp Year</th>
+                <th colspan="2" class="text-center"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($creditcard as $row )
+            <tr>
+                  
+                    <td>{{$row['payment_id']}}</td>
+                    <td>{{$row['name']}}</td>
+                    <td>{{$row['email']}}</td>
+                    <td>{{$row['paid_amount']}}</td>
+                    <td>{{$row['card_number']}}</td>
+                    <td>{{$row['card_cvc']}}</td>
+                    <td>{{$row['card_exp_month']}}</td>
+                    <td>{{$row['card_exp_year']}}</td>
+                </tr>
+                @endforeach
+        </tbody>
+      </table>
+    
+</div>
+          
+@endsection
 
